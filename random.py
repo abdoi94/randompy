392e3b5436a858cba7e2ac33467ae49c310ea87f
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-05 14:21:07 +0300

    random: splicing strings
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,6 +1,4 @@
-fruit = 'banana'
-index = 0
-while index < len(fruit):
-    letter = fruit[index]
-    print(index, letter)
-    index = index + 1
+s = 'Monty Python'
+print(s[0:4])
+print(s[6:7])
+print(s[6:20])
