ace9c3bdb1a4dd3441e3b6b5c3b1cd903aedcce6
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-05 11:07:25 +0300

    random: find smallest
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,7 +1,7 @@
-largest = -1
-print('Before', largest)
+smallest = -1
+print('Before', smallest)
 for num in [9, 41, 12, 3, 74, 15]:
-    if num > largest:
-       largest = num
-       print(largest, num) 
-print('After', largest)
+    if num < smallest:
+        smallest = num
+        print(smallest, num) 
+print('After', smallest)
