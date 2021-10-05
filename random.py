60453cff92e50a56c4c5ae4387d629b85e672359
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-05 10:45:02 +0300

    random: finding average in loop
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,8 +1,8 @@
-smallest = None
-print("Before:", smallest)
-for itervar in [3, 41, 12, 9, 74, 15]:
-    if smallest is None or itervar < smallest:
-        smallest = itervar;
-        break
-    print("Loop:", itervar, smallest)
-print("Smallest:", smallest)
+count = 0
+sum = 0
+print('Before', count, sum)
+for value in [9, 41, 12, 3, 74, 15]:
+    count = count + 1
+    sum = sum + value
+    print(count, sum, value)
+print('After', count, sum, sum/count)
