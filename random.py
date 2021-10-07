f55b1d11ad6b2fdafdbc811bff4a744ea2267664
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-07 10:38:23 +0300

    random: read whole file and print length and part of the txt
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,5 +1,4 @@
-fhand = open('mbot.txt')
-count = 0
-for line in fhand:
-    count = count + 1
-print('Line Count:', count)
+fhand = open('mbot.txt')    #Open txt file
+inp = fhand.read()          #Read txt file
+print(len(inp))             #Print File text length
+print('\n' + inp[:20])      #Print from file start to 20
