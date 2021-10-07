a4c65e9b5ac5d1d2967c14d4af1f0d496cd7f6fd
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-07 12:05:02 +0300

    random: find subjects inside fixed-file
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,5 +1,6 @@
-fhand = open('mbot.txt')    #Open txt file
+fhand = open('mbot.txt')                                              #Open txt file
+count = 0                                                             #Define count to use later
 for line in fhand:
-    line = line.rstrip()          #Strip whitespaces from lines
-    if line.startswith('Hello') : #Find line starting with hello
-        print(line)               #Print those hello lines
+     if line.startswith('Hello') :                                    #Find line starting with hello
+          count = count + 1                                           #Count those lines
+print('There was', count, 'subject lines in:\n', fhand)               #Print subjects count
