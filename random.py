d02a796b92d02499ca74227ce9a603afdabf7544
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-07 12:07:16 +0300

    random: find subjects inside input file
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,6 +1,7 @@
-fhand = open('mbot.txt')                                              #Open txt file
+fname = input('Enter the file name: ')
+fhand = open(fname)                                              #Open txt file
 count = 0                                                             #Define count to use later
 for line in fhand:
      if line.startswith('Hello') :                                    #Find line starting with hello
           count = count + 1                                           #Count those lines
-print('There was', count, 'subject lines in:\n', fhand)               #Print subjects count
+print('There was', count, 'subject lines in:\n', fname)               #Print subjects count
