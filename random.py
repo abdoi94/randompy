49755a01d1fd64cd2cb775ed944e0c7d627964c0
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-07 10:52:51 +0300

    random: searching through a file and print lines without whitespaces
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,4 +1,5 @@
 fhand = open('mbot.txt')    #Open txt file
 for line in fhand:
+    line = line.rstrip()          #Strip whitespaces from lines
     if line.startswith('Hello') : #Find line starting with hello
         print(line)               #Print those hello lines
