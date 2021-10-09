992a67cbca2c7636f308abaa7a4598a2c502ac9c
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-09 12:18:10 +0300

    random: slicing a list
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,13 +1,14 @@
 import sys
 sys.stdout = open("log.txt", "w")
 
-########        #######
-## Concatenate lists ##
-########        #######
+#####           #####
+## slicing a list  ##
+#####           #####
 
-a = [1, 2, 3]
-b = [4, 5, 6]
-c = a + b
-print(c)
+a = [18, 24, 46, 10, 15, 20, 40]
+print(a[:2])                        ## Print out 0 to 2 ##
+print(a[2:5])                       ## Print out 2 to 5 ##
+print(a[0:4])                       ## Print out 0 to 4 ##
+print(a[:])                         ## Print out the whole list ##
 
 sys.stdout.close()
