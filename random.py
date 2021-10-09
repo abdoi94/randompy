91288e7919457c68d649fe494275286bd9167b24
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-09 11:48:15 +0300

    random: print out certain Item in a list
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,22 +1,15 @@
 import sys
 sys.stdout = open("log.txt", "w")
 
-########                  ########
-## Print out all Items in a list##
-########                  ########
-list = [5, 4, 3, 2, 1]
-for i in list:
-     print(i)
-print('Lets go! \n')
-
+########                      ########
+## Print out certain Item in a list##
+########                      ########
+friends = ['Ali', 'Saif', 'Ahmed']
+print(friends[2])
 
 ########                             ########
 ## Print out a text with Items from a list ##
 ########                             ########
 
-z = ['Ali', 'Saif', 'Ahmed']
-for x in z:
-     print('Happy Eid:', x)
-print('Done! \n')
 
 sys.stdout.close()
