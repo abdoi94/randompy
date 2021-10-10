0cb40ea603b406d4eefaa8eae79eed6346a9de19
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-10 10:07:11 +0300

    random: input a list then find the average
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,15 +1,14 @@
 import sys
 sys.stdout = open("log.txt", "w")
 
-#####                   #####
-## builtin list functions  ##
-#####                   #####
+numlist = list()
+while True :
+     inp = input('Please Enter a number: \n')
+     if inp == 'done' : break
+     value = float(inp)
+     numlist.append(value)
 
-nums = [3, 41, 12, 9, 74, 15]
-print(len(nums))
-print(max(nums))
-print(min(nums))
-print(sum(nums))
-print(sum(nums)/len(nums))
+average = sum(numlist) / len(numlist)
+print('\nAverage:', average)
 
 sys.stdout.close()
