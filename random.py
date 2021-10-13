8a266562579100cc64579d78130e1172f81fd9b1
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-13 11:45:21 +0300

    random: count words in input and print out a dict
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -2,9 +2,12 @@ import sys
 sys.stdout = open("log.txt", "w")
 
 counts = dict()
-names = ['csev', 'cwen', 'gwem', 'csev', 'cwen']
-for name in names :
-    counts[name] = counts.get(name, 0) + 1
-print(counts)
+line = input('Enter a line of text:')
+words = line.split()
+print('Words:', words)
+print('\nCounting...\n')
+for word in words:
+    counts[word] = counts.get(word,0) + 1
+print('Counts', counts)
 
 sys.stdout.close()
