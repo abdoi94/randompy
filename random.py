ecd37df734f62c8d5ee404324f9237caabc5eb53
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-13 14:35:09 +0300

    random: print out keys and their values from a set dict
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -3,12 +3,7 @@ sys.stdout = open("log.txt", "w")
 
 counts = { 'chuck' : 1 , 'fred' : 42 , 'jan' : 100 }
 
-for key in counts:            ## print keys with their values
-    print(key, counts[key])
-
-print(list(counts))
-print(counts.keys())
-print(counts.values())
-print(counts.items())
+for aaa,bbb in counts.items():         
+    print(aaa, bbb)
 
 sys.stdout.close()
