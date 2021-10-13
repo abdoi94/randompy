b5141b34c20fae6f981d6ef98146b813a7ec7325
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-13 09:44:47 +0300

    random: basic functions on dict's
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -5,6 +5,11 @@ purse = dict()
 purse['money'] = 12
 purse['candy'] = 3
 purse['tissues'] = 75
-print(purse)
+print('Original:', purse, '\n')
+
+print('there is', purse['candy'], 'Candies in the purse', '\n')
+
+purse['candy'] = purse['candy'] + 2
+print('Edited Dict:', purse, '\n')
 
 sys.stdout.close()
