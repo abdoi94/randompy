27dca63af0ec45e08a57f4a997001fc91b679d1f
Abdulwahab Isam <abdoi94.iq@gmail.com>
2021-10-14 16:36:50 +0300

    random: import a file and find the 10 most used words using tuples
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- random.py
+++ random.py
@@ -1,9 +1,21 @@
 import sys
+from typing import Counter
 sys.stdout = open("log.txt", "w")
 
-b = {'a':10, 'b':1, 'c':22}
-t = sorted(b.items())
-for k, v in t:
-    print(k, v)
+fhand = open('words.txt')
+counts = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+lst = list()
+for key, val in counts.items():
+    newtup = (val, key)
+    lst.append(newtup)
+
+lst = sorted(lst, reverse=True)
+for val, key in lst[:10]:
+    print(key, val)
 
 sys.stdout.close()
